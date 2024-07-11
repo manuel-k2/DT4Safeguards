8eee8fc4798dcb23dc32c30de03e645c4df1c02a
manuel-k2 <m.kreutle@posteo.de>
2024-07-11 15:54:25 +0200

    black formatted

--- project/model/historyclass.py
+++ project/model/historyclass.py
@@ -7,7 +7,7 @@ from model.monitoringsystem import IDClass
 class HistoryClass(IDClass):
     """
     The base class for all instances that come with a history that
-    	tracks all changes made to the instance,
+        tracks all changes made to the instance,
 
     Attributes:
     param (str): An additional parameter specific to HistoryClass.
